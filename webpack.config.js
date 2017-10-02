04394e5c1c21d7371b901f2e254bf4137fcf8286
Alexander Bareyko <energizer@yandex-team.ru>
2017-10-02 21:08:21 +0300

    lesson 3 finals

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const path = require('path')
+
+module.exports = {
+	entry: './source/client/index.js',
+	output: {
+		path: path.resolve(__dirname, '..', '..', 'build'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: [
+					'eslint-loader',
+					'babel-loader'
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'postcss-loader'
+				]
+			}
+		]
+	},
+	plugins: [new HtmlWebpackPlugin()]
+}
