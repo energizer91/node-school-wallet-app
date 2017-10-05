3d50199f88beb9c41cad3140ad6410cddb6b967f
Alexander Bareyko <energizer@yandex-team.ru>
2017-10-05 19:33:29 +0300

    fix emotions hydrate

--- source/client/index.js
+++ source/client/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {hydrate} from 'emotion';
 import {App} from './components';
 
-const {ids} = window.__data;
+const ids = window.__data; //eslint-disable-line
 
 hydrate(ids);
 ReactDOM.render(<App />, document.getElementById('root'));
